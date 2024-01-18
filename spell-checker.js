5d84cb3d9e7f95a578b21522814d8074d2b65713
Danny North <iamdnorth@gmail.com>
2024-01-17 23:22:40 -0700

    Add an extremely simplistic version of the spell checker logic that can handle simple cases

--- spell-checker.js
+++ spell-checker.js
@@ -1,3 +1,8 @@
 export const checkSpelling = (dictionaryList, wordList) => {
-    return 'There are no misspelled words! Congrats!';
+    const misspelledWordList = wordList.filter(word => checkSpellingOfWord(dictionaryList, word))
+    return misspelledWordList.length ? misspelledWordList.join(', ') : 'There are no misspelled words! Congrats!';
+}
+
+const checkSpellingOfWord = (dictionaryList, word) => {
+    return !dictionaryList.includes(word);
 }
--- spell-checker.spec.js
+++ spell-checker.spec.js
@@ -1,15 +1,42 @@
+import fs from 'fs';
 import { checkSpelling } from './spell-checker'
 
 describe('spell-checker', () => {
-    describe('checkSpelling', () => {
-        describe('when there are no misspelled words', () => {
-            const simpleDictionary = ['hello', 'world']
-            const simpleWordList = ['wold']
-            const actual = checkSpelling(simpleDictionary, simpleWordList)
+    const fullDictionary = fs.readFileSync('dictionary.txt', 'utf8').split('\n');
 
-            it('should output that there are no misspelled words', () => {
-                const expected = 'There are no misspelled words! Congrats!'
-                expect(expected).toBe(actual)
+
+    describe('checkSpelling', () => {
+        describe('simple use cases', () => {
+            describe('when there are no misspelled words', () => {
+                const simpleDictionary = ['hello', 'world']
+                const simpleWordList = ['world']
+                const actual = checkSpelling(simpleDictionary, simpleWordList)
+    
+                it('should output that there are no misspelled words', () => {
+                    const expected = 'There are no misspelled words! Congrats!'
+                    expect(actual).toEqual(expected)
+                })
+            })
+            describe('when there is a misspelled word', () => {
+                const simpleDictionary = ['hello', 'world']
+                const misspelledWordList = ['hello', 'wold']
+                const actual = checkSpelling(simpleDictionary, misspelledWordList)
+
+                it('should output the misspelled word', () => {
+                    const expected = 'wold'
+                    expect(actual).toEqual(expected)
+                })
+            })
+
+            describe('when there are multiple misspelled words', () => {
+                const simpleDictionary = ['hello', 'world']
+                const misspelledWordList = ['hello', 'wold', 'huld']
+                const actual = checkSpelling(simpleDictionary, misspelledWordList)
+
+                it('should output the misspelled words in a list joined by a comma', () => {
+                    const expected = 'wold, huld'
+                    expect(actual).toEqual(expected)
+                })
             })
         })
     })
